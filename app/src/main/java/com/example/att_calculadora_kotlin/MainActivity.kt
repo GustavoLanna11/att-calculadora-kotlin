7245f2c1139546658b1f24b95a0652df38eecbe8
DSM5 <fatec-dsm5@academico.intra>
2026-03-24 20:14:26 -0300

    :wrench: fix: ajustes nos imports

--- app/src/main/java/com/example/att_calculadora_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/att_calculadora_kotlin/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+
+// Imports do Compose para Layout
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+
+// Import do tema
 import com.example.att_calculadora_kotlin.ui.theme.AttcalculadorakotlinTheme
 
 class MainActivity : ComponentActivity() {
